0eda82fd38e8ed3cb2338f28e4bd4e89b9dfe294
vishal0202 <ravulakolvishal@gmail.com>
2021-01-30 13:44:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+import pyautogui as pg
+import time
+pg.hotkey('alt','tab')
+capture = cv2.VideoCapture(0)
+capture.set(3, 640)
+capture.set(4, 480)
+
+while True:
+    success, frame = capture.read()
+    frame = cv2.flip(frame,1)
+    cv2.rectangle(frame,(340,180),(540,380),(0,255,0))
+    aoi = frame[180:380,340:540]
+    hsv = cv2.cvtColor(aoi,cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv,(0,20,70),(20,255,255))
+    mask = cv2.dilate(mask,(5,5),iterations=10)
+    mask = cv2.GaussianBlur(mask, (3,3), cv2.BORDER_DEFAULT)
+    contour, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    if len(list(contour))>0:
+        hand = max(contour, key=lambda x: cv2.contourArea(x))
+        hand_area = cv2.contourArea(hand)
+        hull = [cv2.convexHull(i) for i in contour]
+        hull_area = [cv2.contourArea(i) for i in hull]
+        if hand_area>14000:
+            cv2.drawContours(aoi,hull,-1,(0,0,0))
+            area_ratio = (max(hull_area) / hand_area)
+            if len(hull_area)==0:
+                pass
+            else:
+                if area_ratio>1.3:
+                    print('pause')
+                    pg.press('space')
+                    time.sleep(0.5)
+                elif area_ratio>1and area_ratio<1.3:
+                    print('mute')
+                    pg.press('m')
+                    time.sleep(0.5)
+    else:
+        pass
+    cv2.imshow('mask', mask)
+    cv2.imshow("webcam", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
